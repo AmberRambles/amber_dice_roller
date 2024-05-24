d5cca8cd7e947a50ca6beaad9b4bef558e0d0fdb
Amber Shifflett <amber.z.shifflett@outlook.com>
2024-05-24 11:43:17 -0700

    Defines roll_die()

--- dice_roller.py
+++ dice_roller.py
@@ -1,6 +1,7 @@
 #dice_roller.py
 # Coded by Amber Shifflett in May 2024
 print('Dice Roller by Amber Shifflett')
+import sys
 import random
 from time import sleep
 
@@ -10,6 +11,14 @@ while a < 5:
     random.randint(0, 10)
     a += 1
     sleep(0.3)
+
+def roll_die(numSides):
+    try:
+        numSides = int(numSides)
+    catch:
+        print('Error: not a number')
+        sys.exit(1)
+    result = random.randint(1, numSides)
 
 #running = True
 #while running:
